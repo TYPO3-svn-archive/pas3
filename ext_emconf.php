40d06cc7dfa19dd5ac4746c1356f42722fba563f
mipmip <mipmip@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-27 20:46:14 +0000

    import of the community skin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pas3/trunk/pas3-mrm-demo/skin.extbundle/lwtpl_p3communityskin@58659 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "lwtpl_p3communityskin".
+#
+# Auto generated 21-02-2012 13:38
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'PAS3 Community Skin',
+	'description' => 'PAS3 MRM Comminity Skin. The skin is used for the application.',
+	'category' => 'templates',
+	'author' => 'Pim Snel',
+	'author_email' => 'pim@lingewoud.nl',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"b68c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"cd2a";s:20:"doc/wizard_form.html";s:4:"7e46";}',
+);
+
+?>
